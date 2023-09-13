1239c97d2b6903dbaac65f2ae2b86c7e1206c108
Takitafu <mledilem@gmail.com>
2023-09-13 05:08:34 -0700

    Program thst prints using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
 *main - Entry point
 *Return: Always 0 (SUCCESS)
 */
-
 int main(void)
 {
         printf("with proper grammar, but the outcome is a piece of art,\n");
